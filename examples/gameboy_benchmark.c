214423e0e76e7b8f33a9cd92a61a66afe69ffc1c
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2018-09-11 20:25:49 +0100

    Examples: Add benchmark example
    Measures execution speed of blarrgs CPU test in CPU ticks and frames per
    second (LCD isn't drawn though).
    
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

---
+++ examples/gameboy_benchmark.c
@@ -0,0 +1,87 @@
+/* Import blarrg CPU test ROM. */
+#include "../test/cpu_instrs.h"
+
+#define ENABLE_SOUND 0
+#define ENABLE_LCD 0
+
+/* Import emulator library. */
+#include "../gameboy.h"
+
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * Return byte from blarrg test ROM.
+ */
+uint8_t gb_rom_read(struct gb_t **gb, const uint32_t addr)
+{
+	return cpu_instrs_gb[addr];
+}
+
+/**
+ * Ignore cart RAM writes, since the test doesn't require it.
+ */
+void gb_cart_ram_write(struct gb_t **gb, const uint32_t addr, const uint8_t val)
+{
+	return;
+}
+
+/**
+ * Ignore cart RAM reads, since the test doesn't require it.
+ */
+uint8_t gb_cart_ram_read(struct gb_t **gb, const uint32_t addr)
+{
+	return 0xFF;
+}
+
+/**
+ * Ignore all errors.
+ */
+void gb_error(struct gb_t **gb, const enum gb_error_e gb_err)
+{
+	return;
+}
+
+int main(void)
+{
+	unsigned long long benchmark_ticks_total = 0;
+	unsigned long long benchmark_fps_total = 0;
+	struct gb_t gb;
+	const unsigned short pc_end = 0x06F1; /* Test ends when PC is this value. */
+
+	puts("Benchmark started");
+
+	for(unsigned int i = 0; i < 5; i++)
+	{
+		/* Start benchmark. */
+		const unsigned long long start_time = (unsigned long)clock();
+		unsigned long long bench_ticks;
+		unsigned long long bench_fps;
+		unsigned long long frames = 0;
+
+		/* Run ROM test. */
+    	gb = gb_init(&gb_rom_read, &gb_cart_ram_read, &gb_cart_ram_write,
+			&gb_error, NULL);
+
+		/* Step CPU until test is complete. */
+		while(gb.cpu_reg.pc != pc_end)
+		{
+			gb_run_frame(&gb);
+			frames++;
+		}
+
+		/* End benchmark. */
+		bench_ticks = (unsigned long long)clock() - start_time;
+		benchmark_ticks_total += bench_ticks;
+
+		bench_fps = frames / ((double)bench_ticks / CLOCKS_PER_SEC);
+		benchmark_fps_total += bench_fps;
+
+		printf("Benchmark %i: %lld\tFPS: %lld\n", i, bench_ticks, bench_fps);
+	}
+
+	printf("Average    : %lld\tFPS: %lld\n",
+			benchmark_ticks_total / 5, benchmark_fps_total / 5);
+
+	return 0;
+}
