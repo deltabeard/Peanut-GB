932dd76d465a8020ffea9761d70a00372a46e895
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2018-09-11 20:25:23 +0100

    Test: add test framework
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

---
+++ test/minctest.h
@@ -0,0 +1,137 @@
+/*
+ *
+ * MINCTEST - Minimal C Test Library - 0.2.0
+ *
+ * Copyright (c) 2014-2017 Lewis Van Winkle
+ *
+ * http://CodePlea.com
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgement in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ */
+
+
+
+/*
+ * MINCTEST - Minimal testing library for C
+ *
+ *
+ * Example:
+ *
+ *      void test1() {
+ *           lok('a' == 'a');
+ *      }
+ *
+ *      void test2() {
+ *           lequal(5, 6);
+ *           lfequal(5.5, 5.6);
+ *      }
+ *
+ *      int main() {
+ *           lrun("test1", test1);
+ *           lrun("test2", test2);
+ *           lresults();
+ *           return lfails != 0;
+ *      }
+ *
+ *
+ *
+ * Hints:
+ *      All functions/variables start with the letter 'l'.
+ *
+ */
+
+
+#ifndef __MINCTEST_H__
+#define __MINCTEST_H__
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+
+/* How far apart can floats be before we consider them unequal. */
+#ifndef LTEST_FLOAT_TOLERANCE
+#define LTEST_FLOAT_TOLERANCE 0.001
+#endif
+
+
+/* Track the number of passes, fails. */
+/* NB this is made for all tests to be in one file. */
+static int ltests = 0;
+static int lfails = 0;
+
+
+/* Display the test results. */
+#define lresults() do {\
+    if (lfails == 0) {\
+        printf("ALL TESTS PASSED (%d/%d)\n", ltests, ltests);\
+    } else {\
+        printf("SOME TESTS FAILED (%d/%d)\n", ltests-lfails, ltests);\
+    }\
+} while (0)
+
+
+/* Run a test. Name can be any string to print out, test is the function name to call. */
+#define lrun(name, test) do {\
+    const int ts = ltests;\
+    const int fs = lfails;\
+    const clock_t start = clock();\
+    printf("\t%-14s\t", name);\
+    test();\
+    printf("pass:%2d   fail:%2d   %4dms\n",\
+            (ltests-ts)-(lfails-fs), lfails-fs,\
+            (int)((clock() - start) * 1000 / CLOCKS_PER_SEC));\
+} while (0)
+
+
+/* Assert a true statement. */
+#define lok(test) do {\
+    ++ltests;\
+    if (!(test)) {\
+        ++lfails;\
+        printf("%s:%d error\t", __FILE__, __LINE__);\
+    }} while (0)
+
+
+/* Prototype to assert equal. */
+#define lequal_base(equality, a, b, format) do {\
+    ++ltests;\
+    if (!(equality)) {\
+        ++lfails;\
+        printf("%s:%d ("format " != " format")\n", __FILE__, __LINE__, (a), (b));\
+    }} while (0)
+
+
+/* Assert two integers are equal. */
+#define lequal(a, b)\
+    lequal_base((a) == (b), a, b, "%d")
+
+
+/* Assert two floats are equal (Within LTEST_FLOAT_TOLERANCE). */
+#define lfequal(a, b)\
+    lequal_base(fabs((double)(a)-(double)(b)) <= LTEST_FLOAT_TOLERANCE\
+     && fabs((double)(a)-(double)(b)) == fabs((double)(a)-(double)(b)), (double)(a), (double)(b), "%f")
+
+
+/* Assert two strings are equal. */
+#define lsequal(a, b)\
+    lequal_base(strcmp(a, b) == 0, a, b, "%s")
+
+
+#endif /*__MINCTEST_H__*/
---
+++ test/test.c
@@ -0,0 +1,85 @@
+#include "minctest.h"
+#include "cpu_instrs.h"
+#include "../gameboy.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Return byte from blarrg test ROM.
+ */
+uint8_t gb_rom_read(struct gb_t **gb, const uint32_t addr)
+{
+	return cpu_instrs_gb[addr];
+}
+
+/**
+ * Ignore cart RAM writes, since the test doesn't require it.
+ */
+void gb_cart_ram_write(struct gb_t **gb, const uint32_t addr, const uint8_t val)
+{
+	return;
+}
+
+/**
+ * Ignore cart RAM reads, since the test doesn't require it.
+ */
+uint8_t gb_cart_ram_read(struct gb_t **gb, const uint32_t addr)
+{
+	return 0xFF;
+}
+
+/**
+ * Ignore all errors.
+ */
+void gb_error(struct gb_t **gb, const enum gb_error_e gb_err)
+{
+	return;
+}
+
+void test_cpu_inst(void)
+{
+	struct gb_t gb;
+	struct gb_t *p = &gb;
+	char str[1024];
+	unsigned int count = 0;
+	const unsigned short pc_end = 0x06F1; /* Test ends when PC is this value. */
+
+	/* Run ROM test. */
+    *p = gb_init(&gb_rom_read, &gb_cart_ram_read, &gb_cart_ram_write, &gb_error,
+			NULL);
+
+	printf("Serial: ");
+
+	/* Step CPU until test is complete. */
+	while(gb.cpu_reg.pc != pc_end)
+	{
+		__gb_step_cpu(&p);
+
+		/* Detect serial transmission. Test status is pushed to serial by the
+		 * test ROM. */
+		if(gb.gb_reg.SC == 0x81)
+		{
+			printf("%c", gb.gb_reg.SB != '\n' ? gb.gb_reg.SB : ' ');
+			str[count++] = gb.gb_reg.SB;
+			if(count == 1024)
+				abort();
+			
+			/* Simulate serial read, as emulator does not support serial
+			 * transmission yet. */
+			gb.gb_reg.SC = 0x01;
+		}
+	}
+
+	/* Check test results. */
+	lok(strstr(str, "Passed all tests") != NULL);
+
+	return;
+}
+
+int main(void)
+{
+	lrun("cpu_inst blarrg tests", test_cpu_inst);
+	return lfails != 0;
+}
