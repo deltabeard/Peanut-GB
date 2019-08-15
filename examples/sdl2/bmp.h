4f3eab28c97ad0ddd2a48080dcfea70700b23b79
Mahyar Koshkouei <mk@deltabeard.com>
2019-08-15 18:34:42 +0100

    sdl2: Remove unused functions in bmp.h
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- examples/sdl2/bmp.h
+++ examples/sdl2/bmp.h
@@ -6,19 +6,6 @@
 
 #define BMP_SIZE(w, h) ((h) * ((w) + ((w) * 3) % 4) * 3 + 14 + 40)
 
-static unsigned long
-bmp_size(long width, long height)
-{
-    long pad = ((width % 4) * 3) % 4; /* Overflow-safe */
-    if (width < 1 || height < 1) {
-        return 0; /* Illegal size */
-    } else if (width > (0x7fffffffL - 14 - 40) / 3 / height - pad) {
-        return 0; /* Overflow */
-    } else {
-        return height * (width + pad) * 3 + 14 + 40;
-    }
-}
-
 static void
 bmp_init(void *buf, long width, long height)
 {
@@ -126,32 +113,8 @@ bmp_init(void *buf, long width, long height)
 }
 
 static void
-bmp_set(void *buf, long x, long y, unsigned long color)
-{
-    unsigned char *p;
-    unsigned char *hdr = (unsigned char *)buf;
-    unsigned long width =
-        (unsigned long)hdr[18] <<  0 |
-        (unsigned long)hdr[19] <<  8 |
-        (unsigned long)hdr[20] << 16 |
-        (unsigned long)hdr[21] << 24;
-    long pad = (width * 3) % 4;
-#ifdef BMP_COMPAT
-    unsigned long height =
-        (unsigned long)hdr[22] <<  0 |
-        (unsigned long)hdr[23] <<  8 |
-        (unsigned long)hdr[24] << 16 |
-        (unsigned long)hdr[25] << 24;
-    y = height - y - 1;
-#endif
-    p = hdr + 14 + 40 + y * (width + pad) * 3 + x * 3;
-    p[0]  = color >>  0;
-    p[1]  = color >>  8;
-    p[2]  = color >> 16;
-}
-
-static void
-bmp_set_rgb(void *buf, long x, long y, unsigned char r, unsigned char g, unsigned char b)
+bmp_set_rgb(void *buf, long x, long y,
+		unsigned char r, unsigned char g, unsigned char b)
 {
     unsigned char *p;
     unsigned char *hdr = (unsigned char *)buf;
@@ -173,51 +136,6 @@ bmp_set_rgb(void *buf, long x, long y, unsigned char r, unsigned char g, unsigne
     p[0]  = b;
     p[1]  = g;
     p[2]  = r;
-}
-
-static unsigned long
-bmp_get(const void *buf, long x, long y)
-{
-    const unsigned char *p;
-    const unsigned char *hdr = (unsigned char *)buf;
-    unsigned long width =
-        (unsigned long)hdr[18] <<  0 |
-        (unsigned long)hdr[19] <<  8 |
-        (unsigned long)hdr[20] << 16 |
-        (unsigned long)hdr[21] << 24;
-    long pad = (width * 3) % 4;
-#ifdef BMP_COMPAT
-    unsigned long height =
-        (unsigned long)hdr[22] <<  0 |
-        (unsigned long)hdr[23] <<  8 |
-        (unsigned long)hdr[24] << 16 |
-        (unsigned long)hdr[25] << 24;
-    y = height - y - 1;
-#endif
-    p = hdr + 14 + 40 + y * (width + pad) * 3 + x * 3;
-    return (unsigned long)p[0] <<  0 |
-           (unsigned long)p[1] <<  8 |
-           (unsigned long)p[2] << 16;
-}
-
-static unsigned long
-bmp_encode(float r, float g, float b)
-{
-    unsigned long ur = r * 255.0f + 0.5f;
-    unsigned long ug = g * 255.0f + 0.5f;
-    unsigned long ub = b * 255.0f + 0.5f;
-    return ub | ug << 8 | ur << 16;
-}
-
-static void
-bmp_decode(unsigned long c, float *r, float *g, float *b)
-{
-    unsigned long ur = c >> 16 & 0xff;
-    unsigned long ug = c >>  8 & 0xff;
-    unsigned long ub = c >>  0 & 0xff;
-    *r = ur / 255.0f;
-    *g = ug / 255.0f;
-    *b = ub / 255.0f;
 }
 
 #endif /* BMP_H */
