cd2c55716eeeefe85f6211eda9ec7d119b5f8aa9
Mahyar Koshkouei <mk@deltabeard.com>
2023-07-22 11:57:16 +0100

    gb: change Mode 0 timing for DMG as well
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- peanut_gb.h
+++ peanut_gb.h
@@ -195,12 +195,8 @@
 /** LCD characteristics **/
 /* PPU cycles through modes every 456 cycles. */
 #define LCD_LINE_CYCLES     456
-/* Mode 0 starts on cycle 372. */
-#if PEANUT_FULL_GBC_SUPPORT
+/* Mode 0 starts on cycle 380. */
 #define LCD_MODE_0_CYCLES   380
-#else
-#define LCD_MODE_0_CYCLES   372
-#endif
 /* Mode 2 starts on cycle 204. */
 #define LCD_MODE_2_CYCLES   204
 /* Mode 3 starts on cycle 284. */
